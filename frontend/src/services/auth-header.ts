13175470654d5c7fcd66ac61008bfd88165f8c75
刘松铭 <2018011960@secoder.net>
2021-09-26 15:51:48 +0800

    (fix) (frontend #2): Fix wrong key identifier for user data in auth head service

--- frontend/src/services/auth-header.ts
+++ frontend/src/services/auth-header.ts
@@ -7,8 +7,8 @@ export default function authHeader() {
         user = null;
     }
 
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken }; // for Spring Boot back-end
+    if (user && user.token) {
+        return { Authorization: 'Bearer ' + user.token }; // for Spring Boot back-end
         // return { 'x-access-token': user.accessToken };       // for Node.js Express back-end
     } else {
         return {};
