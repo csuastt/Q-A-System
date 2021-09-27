17cae7839293b7b37286d42861ab75214aad8dd7
刘松铭 <2018011960@secoder.net>
2021-09-27 18:12:22 +0800

    (feat) (frontend #2): Add RegisterPage

--- frontend/src/components/registerComponent.tsx
+++ frontend/src/components/registerComponent.tsx
@@ -10,51 +10,45 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {Alert} from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+// email checker
+// @ts-ignore
+import {isEmail} from "validator";
+// other validators
+import {validate_required, validate_length} from './loginComponent'
 
 // theme
 const theme = createTheme();
 
-// some validators
-// not empty
-export const validate_required = (value: any) => {
-    if (!value) {
-        return '此处不能为空';
+// email validator
+// to ensure a valid email
+const validate_email = (value: any) => {
+    if (!isEmail(value.toString().trim())) {
+        return '请输入合法的邮箱';
     } else {
         return '';
     }
 };
-// 6 to 12 in length
-export const validate_length = (value: any) => {
-    if (value.toString().trim().length < 6) {
-        // too long
-        return '长度不能小于6个字符';
-    }
-    else if(value.toString().trim().length > 12) {
-        // too short
-        return '长度不能大于12个字符';
-    }
-    else {
-        return '';
-    }
-};
 
-export default class Login extends Component {
+
+export default class Register extends Component {
 
     constructor(props: any) {
         super(props);
-        // handle login info
-        this.handleLogin = this.handleLogin.bind(this);
+        // handle register info
+        this.handleRegister = this.handleRegister.bind(this);
         // state
         this.state = {
             username: '',
             password: '',
+            email: '',
             error_msg_username: '',
-            error_msg_password: ''
+            error_msg_password: '',
+            error_msg_email: ''
         };
     }
     // if alert
@@ -62,12 +56,15 @@ export default class Login extends Component {
     // alert msg
     private alertContent = '';
 
-    // listener on username/password
-    onChangeValue(e: any, type: 'username' | 'password') {
+    // listener on email/username/password
+    onChangeValue(e: any, type: 'username' | 'password' | 'email') {
         const value = e.target.value;
         // first validate not empty
         let error = validate_required(value);
-        if (error === ''){
+        if (error === '' && type === 'email'){
+            error = validate_email(value);
+        }
+        else if(error === '') {
             error = validate_length(value);
         }
         // set new state
@@ -77,7 +74,7 @@ export default class Login extends Component {
         this.setState(nextState);
     }
 
-    handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    handleRegister(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
         if ((
@@ -88,16 +85,22 @@ export default class Login extends Component {
             // @ts-ignore
             validate_required(this.state.password) +
             // @ts-ignore
-            validate_length(this.state.password)
+            validate_length(this.state.password) +
+            // @ts-ignore
+            validate_required(this.state.email) +
+            // @ts-ignore
+            validate_email(this.state.email)
         ).length === 0) {
             // @ts-ignore
-            AuthService.login(this.state.username, this.state.password).then(
+            AuthService.register(this.state.username,
+                // @ts-ignore
+                this.state.email, this.state.password).then(
                 () => {
                     // login success
                     // todo jump to sw
                     // alert
                     this.alert = true;
-                    this.alertContent = 'Login success!';
+                    this.alertContent = 'Register success!';
                 },
                 error => {
                     // retrieve error
@@ -130,19 +133,19 @@ export default class Login extends Component {
                         }}
                     >
                         <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-                            <LockOutlinedIcon />
+                            <AccountCircleIcon />
                         </Avatar>
                         <Typography component="h1" variant="h5">
-                            登录
+                            注册
                         </Typography>
                         <Box component="form"
-                             onSubmit={this.handleLogin}
+                             onSubmit={this.handleRegister}
                              noValidate sx={{ mt: 1 }}>
                             <TextField
                                 margin="normal"
                                 required
                                 fullWidth
-                                id="username"
+                                id="reg_username"
                                 label="用户名"
                                 name="username"
                                 autoComplete="username"
@@ -158,10 +161,26 @@ export default class Login extends Component {
                                 margin="normal"
                                 required
                                 fullWidth
+                                id="reg_email"
+                                label="邮箱"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={e => this.onChangeValue(e, 'email')}
+                                // @ts-ignore
+                                error={this.state.error_msg_email.length !== 0}
+                                // @ts-ignore
+                                helperText={this.state.error_msg_email}
+                                inputProps={{ maxLength: 30 }}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
                                 name="password"
                                 label="密码"
                                 type="password"
-                                id="password"
+                                id="reg_password"
                                 autoComplete="current-password"
                                 onChange={e => this.onChangeValue(e, 'password')}
                                 // @ts-ignore
@@ -171,8 +190,13 @@ export default class Login extends Component {
                                 inputProps={{ maxLength: 15 }}
                             />
                             <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="记住我"
+                                control={<Checkbox value="agree" color="primary" />}
+                                label={
+                                    <div>
+                                        <span>我已阅读并同意</span>
+                                        <Link href={'https://www.kuaishou.com/about/policy'}>《快手用户服务协议》</Link>
+                                    </div>
+                                }
                             />
                             <Button
                                 type="submit"
@@ -180,17 +204,12 @@ export default class Login extends Component {
                                 variant="contained"
                                 sx={{ mt: 3, mb: 2 }}
                             >
-                                登录
+                                注册
                             </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        忘记密码？
-                                    </Link>
-                                </Grid>
+                            <Grid container justifyContent="flex-end">
                                 <Grid item>
                                     <Link href="#" variant="body2">
-                                        {"还没有账号？快速注册"}
+                                        {"已有账号？快速登录"}
                                     </Link>
                                 </Grid>
                             </Grid>
