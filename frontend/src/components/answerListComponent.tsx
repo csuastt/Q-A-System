d199d3cd67d70598c33381b22492c75439fad206
徐晨曦 <2019010895@secoder.net>
2021-10-13 02:30:56 +0800

    (feat) (frontend #42): add select mode for answerer list

--- frontend/src/components/answerListComponent.tsx
+++ frontend/src/components/answerListComponent.tsx
@@ -5,7 +5,9 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import UserCard from "./userCard";
 
-const AnswerList: React.FC<{ type: string }> = (props) => {
+const AnswererList: React.FC<{ type: string; selectModel?: boolean }> = (
+    props
+) => {
     const [answerList, setAnswerList] = useState<Array<UserBasicInfo>>();
     useEffect(() => {
         userService.getUserList(true).then((list) => setAnswerList(list));
@@ -20,7 +22,14 @@ const AnswerList: React.FC<{ type: string }> = (props) => {
             <>
                 {answerList.map((user: UserBasicInfo, index: number) => (
                     <Grid item key={index} lg={4} md={6} xs={12}>
-                        <UserCard userInfo={user} />
+                        <UserCard
+                            userInfo={user}
+                            nextUrl={
+                                props.selectModel
+                                    ? `/order/create/${user.id}`
+                                    : undefined
+                            }
+                        />
                     </Grid>
                 ))}
             </>
@@ -37,4 +46,4 @@ const AnswerList: React.FC<{ type: string }> = (props) => {
     );
 };
 
-export default AnswerList;
+export default AnswererList;
