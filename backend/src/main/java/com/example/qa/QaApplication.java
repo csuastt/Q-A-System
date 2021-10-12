63ca5c0573008326aea3d7b5801d1356fe65085e
肖震宇 <2019011237@secoder.net>
2021-10-12 10:44:48 +0800

    (fix) (backend #39): fix cors

--- backend/src/main/java/com/example/qa/QaApplication.java
+++ backend/src/main/java/com/example/qa/QaApplication.java
@@ -2,10 +2,27 @@ package com.example.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QaApplication {
     public static void main(String[] args) {
         SpringApplication.run(QaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("PUT", "GET", "POST", "DELETE", "HEAD")
+                        .allowedHeaders("header1", "header2")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
