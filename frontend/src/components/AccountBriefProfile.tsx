988ac98b95c85f2989a81cb89d5ea3b5a728fb67
刘松铭 <2018011960@secoder.net>
2021-10-17 17:48:12 +0800

    (feat) (frontend #52): Add apply Dialog and some handler

--- frontend/src/components/AccountBriefProfile.tsx
+++ frontend/src/components/AccountBriefProfile.tsx
@@ -8,6 +8,12 @@ import CardActions from "@mui/material/CardActions";
 import Divider from "@mui/material/Divider";
 import CardContent from "@mui/material/CardContent";
 import { Component } from "react";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from "@mui/material/TextField";
 
 interface AccountBriefProfileProps {
     avatar: string | undefined;
@@ -21,10 +27,28 @@ interface AccountBriefProfileProps {
     redirectHandler: (arg1: string) => void;
 }
 
+interface AccountBriefProfileState {
+    openApplyDialog: boolean;
+    openPriceDialog: boolean;
+}
+
+
 export default class AccountBriefProfile extends Component<
     AccountBriefProfileProps,
-    any
+    AccountBriefProfileState
 > {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            openApplyDialog: false,
+            openPriceDialog: false
+        };
+        this.handleCloseApplyDialog = this.handleCloseApplyDialog.bind(this);
+        this.handleClosePriceDialog = this.handleClosePriceDialog.bind(this);
+    }
+
     handleLogout() {
         // check if there if the username
         if (!this.props.username) return;
@@ -32,56 +56,96 @@ export default class AccountBriefProfile extends Component<
         this.props.redirectHandler("/logout");
     }
 
+    handleCloseApplyDialog() {
+        this.setState({openApplyDialog: false});
+    }
+
+    handleOpenApplyDialog() {
+        this.setState({openApplyDialog: true});
+    }
+
+    handleClosePriceDialog() {
+        this.setState({openPriceDialog: false});
+    }
+
+    handleOpenPriceDialog() {
+        this.setState({openPriceDialog: true})
+    }
+
     render() {
         return (
-            <Card>
-                <CardContent>
-                    <Box
-                        sx={{
-                            alignItems: "center",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Avatar
-                            src={this.props.avatar}
+            <>
+                <Card>
+                    <CardContent>
+                        <Box
                             sx={{
-                                height: 100,
-                                width: 100,
+                                alignItems: "center",
+                                display: "flex",
+                                flexDirection: "column",
                             }}
+                        >
+                            <Avatar
+                                src={this.props.avatar}
+                                sx={{
+                                    height: 100,
+                                    width: 100,
+                                }}
+                            />
+                            <Box mt={2}>
+                                <Typography
+                                    color="textPrimary"
+                                    gutterBottom
+                                    variant="h4"
+                                >
+                                    {this.props.nickname === ""
+                                        ? "匿名用户"
+                                        : this.props.nickname}
+                                </Typography>
+                            </Box>
+                            <Box mx={2}>
+                                <Typography color="textSecondary" variant="body1">
+                                    {this.props.permission === "q"
+                                        ? "你还不是问答者，快去申请吧~"
+                                        : "你已经是问答者了，快去回答问题吧~"}
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </CardContent>
+                    <Divider />
+                    <CardActions>
+                        <Button
+                            color="error"
+                            fullWidth
+                            variant="text"
+                            onClick={this.handleLogout.bind(this)}
+                        >
+                            退出登录
+                        </Button>
+                    </CardActions>
+                </Card>
+                <Dialog open={this.state.openApplyDialog} onClose={this.handleCloseApplyDialog}>
+                    <DialogTitle>Subscribe</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To subscribe to this website, please enter your email address here. We
+                            will send updates occasionally.
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                            variant="standard"
                         />
-                        <Box mt={2}>
-                            <Typography
-                                color="textPrimary"
-                                gutterBottom
-                                variant="h4"
-                            >
-                                {this.props.nickname === ""
-                                    ? "匿名用户"
-                                    : this.props.nickname}
-                            </Typography>
-                        </Box>
-                        <Box mx={2}>
-                            <Typography color="textSecondary" variant="body1">
-                                {this.props.permission === "q"
-                                    ? "你还不是问答者，快去申请吧~"
-                                    : "你已经是问答者了，快去回答问题吧~"}
-                            </Typography>
-                        </Box>
-                    </Box>
-                </CardContent>
-                <Divider />
-                <CardActions>
-                    <Button
-                        color="error"
-                        fullWidth
-                        variant="text"
-                        onClick={this.handleLogout.bind(this)}
-                    >
-                        退出登录
-                    </Button>
-                </CardActions>
-            </Card>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleCloseApplyDialog}>Cancel</Button>
+                        <Button onClick={this.handleCloseApplyDialog}>Subscribe</Button>
+                    </DialogActions>
+                </Dialog>
+            </>
         );
     }
 }
