e443f3b8c6a433508f4cf3ebc11aa4045d17f34a
孙天乐 <2020010907@secoder.net>
2021-10-20 09:05:13 +0000

    (refactor) (backend #51) refactor SpringSecurityConfiguration

--- backend/src/main/java/com/example/qa/security/SpringSecurityConfiguration.java
+++ backend/src/main/java/com/example/qa/security/SpringSecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.example.qa.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,29 +12,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SpringSecurityConfiguration {
-    public SpringSecurityConfiguration() {
+@Configuration
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Configuration
-    @Order(1)
-    public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .cors().and()
-                    .csrf().disable()
-                    .authorizeRequests()
-                    .anyRequest().permitAll()
-                    .and()
-                    .addFilter(new JwtAuthorizationFilter(authenticationManager()))
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
