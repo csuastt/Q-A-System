c8bd0688789dcce27e517a65a585445b8396834b
徐晨曦 <2019010895@secoder.net>
2021-10-21 20:09:58 +0800

    (feat) (frontend #56): add answerer detail dialog

--- frontend/src/components/AnswererCard.tsx
+++ frontend/src/components/AnswererCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
-import { UserBasicInfo, UserType } from "../services/definations";
+import { UserBasicInfo } from "../services/definations";
 import userService from "../services/user.service";
 import CardHeader from "@mui/material/CardHeader";
 import Avatar from "@mui/material/Avatar";
@@ -13,6 +13,7 @@ import Box from "@mui/material/Box";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import { Grid } from "@mui/material";
+import AnswererDetailDialog from "./AnswererDetailDialog";
 
 const AnswererCard: React.FC<{
     userInfo?: UserBasicInfo;
@@ -21,6 +22,7 @@ const AnswererCard: React.FC<{
     nextUrl?: string;
 }> = (props) => {
     const [userInfo, setUserInfo] = useState<UserBasicInfo>();
+    const [dialogOpen, setDialogOpen] = useState(false);
 
     useEffect(() => {
         if (props.placeholder) {
@@ -34,99 +36,135 @@ const AnswererCard: React.FC<{
         }
     }, [props.placeholder, props.userId, props.userInfo]);
 
+    const handleOpenDialog = () => setDialogOpen(true);
+    const handleCloseDialog = () => setDialogOpen(false);
+
     const CardActionWrapper: React.FC<{ nextUrl?: string }> = (
         wrapperProps
     ) => {
         return wrapperProps.nextUrl ? (
+            // If has nextUrl, Card will follow it
             <CardActionArea component={RouterLink} to={wrapperProps.nextUrl}>
                 {wrapperProps.children}
             </CardActionArea>
         ) : (
-            <>{wrapperProps.children}</>
+            // Or it will show answerer's detail dialog
+            <CardActionArea onClick={handleOpenDialog}>
+                {wrapperProps.children}
+            </CardActionArea>
         );
     };
 
     return userInfo ? (
-        <Card>
-            <CardActionWrapper nextUrl={props.nextUrl}>
-                <CardContent>
-                    <Box
-                        sx={{
-                            alignItems: "center",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Avatar
-                            alt={userInfo.username}
-                            src={userInfo.ava_url}
+        <>
+            <Card>
+                <CardActionWrapper nextUrl={props.nextUrl}>
+                    <CardContent sx={{ paddingBottom: 0 }}>
+                        <Box
                             sx={{
-                                height: 70,
-                                width: 70,
+                                alignItems: "center",
+                                display: "flex",
+                                flexDirection: "column",
                             }}
-                        />
-                        <Box mt={1}>
-                            <Typography
-                                color="textPrimary"
-                                gutterBottom
-                                variant="h5"
-                            >
-                                {userInfo.username}
-                            </Typography>
-                        </Box>
-                        <Box mx={2} mt={-1}>
-                            <Typography color="textSecondary" variant="body1">
-                                {userInfo.description}
-                            </Typography>
-                        </Box>
-                        {userInfo.type === 1 ? (
-                            <Grid
-                                container
-                                mt={0.5}
-                                direction="row"
-                                justifyContent="center"
-                                alignItems="flex-end"
-                            >
-                                <Grid item>
-                                    <Typography color="primary" variant="h6">
-                                        ￥
-                                    </Typography>
+                        >
+                            <Avatar
+                                alt={userInfo.username}
+                                src={userInfo.ava_url}
+                                sx={{
+                                    height: 70,
+                                    width: 70,
+                                }}
+                            />
+                            <Box mt={1}>
+                                <Typography
+                                    color="textPrimary"
+                                    gutterBottom
+                                    variant="h5"
+                                >
+                                    {userInfo.username}
+                                </Typography>
+                            </Box>
+                            <Box mx={2} mt={-1}>
+                                <Typography
+                                    color="textSecondary"
+                                    variant="body1"
+                                >
+                                    {userInfo.description}
+                                </Typography>
+                            </Box>
+                            {userInfo.type === 1 ? (
+                                <Grid
+                                    container
+                                    mt={0.5}
+                                    direction="row"
+                                    justifyContent="center"
+                                    alignItems="flex-end"
+                                >
+                                    <Grid item>
+                                        <Typography
+                                            color="primary"
+                                            variant="h6"
+                                        >
+                                            ￥
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography
+                                            color="primary"
+                                            variant="h4"
+                                        >
+                                            49.9
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography
+                                            color="primary"
+                                            variant="h6"
+                                        >
+                                            /次
+                                        </Typography>
+                                    </Grid>
                                 </Grid>
-                                <Grid item>
-                                    <Typography color="primary" variant="h4">
-                                        49.9
-                                    </Typography>
-                                </Grid>
-                                <Grid item>
-                                    <Typography color="primary" variant="h6">
-                                        /次
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                        ) : (
-                            <></>
-                        )}
-                    </Box>
-                </CardContent>
-                {userInfo.type === UserType.Answerer && props.nextUrl ? (
-                    <CardActions style={{ justifyContent: "center" }}>
-                        <Box mb={1.5} mt={-2}>
-                            <Button
-                                color="primary"
-                                size="large"
-                                variant="contained"
-                                component={RouterLink}
-                                to={props.nextUrl}
-                            >
-                                向TA提问
-                            </Button>
+                            ) : (
+                                <></>
+                            )}
                         </Box>
-                    </CardActions>
-                ) : (
-                    <></>
-                )}
-            </CardActionWrapper>
-        </Card>
+                    </CardContent>
+                </CardActionWrapper>
+                <CardActions
+                    style={{ justifyContent: "center" }}
+                    sx={{ paddingBottom: 2 }}
+                >
+                    <Button
+                        color="primary"
+                        size="large"
+                        variant="outlined"
+                        onClick={handleOpenDialog}
+                    >
+                        详细信息
+                    </Button>
+                    {props.nextUrl && (
+                        <Button
+                            color="primary"
+                            size="large"
+                            variant="contained"
+                            component={RouterLink}
+                            to={props.nextUrl}
+                            sx={{ ml: 2 }}
+                        >
+                            向TA提问
+                        </Button>
+                    )}
+                </CardActions>
+            </Card>
+            <AnswererDetailDialog
+                open={dialogOpen}
+                onClose={handleCloseDialog}
+                info={userInfo}
+                maxWidth="sm"
+                fullWidth
+            />
+        </>
     ) : (
         <Card>
             <CardHeader
---
+++ frontend/src/components/AnswererDetailDialog.tsx
@@ -0,0 +1,46 @@
+import Dialog, { DialogProps } from "@mui/material/Dialog";
+import { UserBasicInfo } from "../services/definations";
+import React from "react";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import CardContent from "@mui/material/CardContent";
+
+const AnswererDetailDialog: React.FC<DialogProps & { info: UserBasicInfo }> = (
+    props
+) => {
+    const { info } = props;
+    return (
+        <Dialog {...props}>
+            <DialogTitle>回答者信息</DialogTitle>
+            <DialogContent>
+                <Card elevation={0}>
+                    <CardHeader
+                        avatar={
+                            <Avatar alt={info.username} src={info.ava_url} />
+                        }
+                        title={info.nickname ? info.nickname : info.username}
+                        subheader={info.nickname && "@" + info.username}
+                    />
+                    <CardContent>
+                        <TextField
+                            label="个人介绍"
+                            contentEditable={false}
+                            value={
+                                info.description
+                                    ? info.description
+                                    : "该回答者很懒～还没有填写个人介绍哦～"
+                            }
+                            fullWidth
+                        />
+                    </CardContent>
+                </Card>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default AnswererDetailDialog;
