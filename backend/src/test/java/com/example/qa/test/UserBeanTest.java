db9dae89d83457f1d71af43f962b4a2c536ce870
肖震宇 <2019011237@secoder.net>
2021-10-29 17:01:18 +0800

    (enhancement) (backend #82): add UserBean Test

--- backend/src/test/java/com/example/qa/test/UserBeanTest.java
+++ backend/src/test/java/com/example/qa/test/UserBeanTest.java
@@ -1,14 +1,34 @@
 package com.example.qa.test;
 
+import com.example.qa.user.exchange.UserListResponse;
+import com.example.qa.user.exchange.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-public class UserBeanTest {
+class UserBeanTest {
 
     @Test
     void testSetProperty(){
 
         //Test for UserListResponse
+        int pageSize = 1;
+        int page = 1;
+        int totalPages = 1;
+        long totalCount = 1;
+        UserResponse[] data = new UserResponse[]{};
+        UserListResponse response = new UserListResponse();
+        response.setPage(page);
+        response.setPageSize(pageSize);
+        response.setTotalPages(totalPages);
+        response.setTotalCount(totalCount);
+        response.setData(data);
+        assertEquals(page, response.getPage());
+        assertEquals(pageSize, response.getPageSize());
+        assertEquals(totalPages, response.getTotalPages());
+        assertEquals(totalCount, response.getTotalCount());
+        assertEquals(data, response.getData());
     }
 }
