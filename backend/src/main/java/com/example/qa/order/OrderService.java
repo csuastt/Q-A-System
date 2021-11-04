cedb224226506d430b745723643ee806040fe1c5
孙天乐 <2020010907@secoder.net>
2021-11-04 03:02:45 +0000

    (fix) (backend #97) remove unnecessary import

--- backend/src/main/java/com/example/qa/order/OrderService.java
+++ backend/src/main/java/com/example/qa/order/OrderService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.lang.NullPointerException;
 
 @Service
 public class OrderService {
