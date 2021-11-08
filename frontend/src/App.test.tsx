c1aff62c8ee1097e96fdab73cf3671cc3a11cc1d
徐晨曦 <2019010895@secoder.net>
2021-11-08 21:50:38 +0800

    (fix) (frontend #114): fix test error by mocking empty implement for apexcharts

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+jest.mock("react-apexcharts", () => {
+    return {
+        __esModule: true,
+        default: () => {
+            return null;
+        },
+    };
+});
+
 test("renders learn react link", async () => {
     render(<App />);
     const linkElement = await screen.findByText(/Hi，是求知让我们相聚于此/i);
