3725fe964ec028d765b1b25c1e26a9a52d030911
刘松铭 <2018011960@secoder.net>
2021-11-10 17:14:15 +0800

    (fix) (frontend #123): fix one more code smell

--- frontend/src/components/AppFrame.tsx
+++ frontend/src/components/AppFrame.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
 import { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import {
-    alpha,
-    CSSObject,
-    styled,
-    Theme,
-    useTheme,
-} from "@mui/material/styles";
+import { CSSObject, styled, Theme, useTheme } from "@mui/material/styles";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
 import MuiDrawer from "@mui/material/Drawer";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
