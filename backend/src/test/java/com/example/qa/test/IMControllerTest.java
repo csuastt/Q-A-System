64173f4fa9f9f7a9f44a3ec26da22807dc8174c5
肖震宇 <2019011237@secoder.net>
2021-11-10 11:16:05 +0800

    (test) (backend #122): add part of IMControllerTest

--- backend/src/test/java/com/example/qa/test/IMControllerTest.java
+++ backend/src/test/java/com/example/qa/test/IMControllerTest.java
@@ -4,11 +4,15 @@ import com.example.qa.admin.AdminRepository;
 import com.example.qa.admin.exchange.AdminRequest;
 import com.example.qa.admin.model.Admin;
 import com.example.qa.admin.model.AdminRole;
+import com.example.qa.errorhandling.MessageException;
 import com.example.qa.exchange.LoginRequest;
 import com.example.qa.exchange.TokenResponse;
 import com.example.qa.im.IMService;
 import com.example.qa.notification.NotificationRepository;
 import com.example.qa.order.OrderRepository;
+import com.example.qa.order.exchange.OrderRequest;
+import com.example.qa.order.exchange.OrderResponse;
+import com.example.qa.order.model.Order;
 import com.example.qa.security.SecurityConstants;
 import com.example.qa.user.UserRepository;
 import com.example.qa.user.exchange.RegisterRequest;
@@ -22,6 +26,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.ZonedDateTime;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,7 +51,14 @@ class IMControllerTest {
 
     private static String askerToken;
     private static String answererToken;
+    private static long askerId2;
+    private static long answererId2;
     private static String superAdminToken;
+
+    private static User asker;
+    private static User answerer;
+    private static String askerToken2;
+    private static String answererToken2;
 
 
     IMControllerTest(@Autowired IMService imService, @Autowired NotificationRepository notificationRepository,
@@ -58,6 +71,24 @@ class IMControllerTest {
         userRepo = userRepository;
         adminRepo = adminRepository;
     }
+
+    @Test
+    long createOrder() throws Exception {
+        OrderRequest request = new OrderRequest();
+        request.setAsker(askerId);
+        request.setAnswerer(answererId);
+        request.setTitle(question);
+        request.setDescription(description);
+        OrderResponse result = mockUtils.postAndDeserialize("/api/orders", askerToken, request, status().isOk(), OrderResponse.class);
+        mockUtils.postUrl("/api/orders",answererToken, request, status().isForbidden());
+//        mockUtils.postUrl("/api/orders",answererToken2, request, status().isForbidden());
+//        mockUtils.postUrl("/api/orders",askerToken2, request, status().isForbidden());
+//        mockUtils.postUrl("/api/orders",superAdminToken, request, status().isOk());
+//        mockUtils.postUrl("/api/orders",adminToken, request, status().isOk());
+
+        return result.getId();
+    }
+
     @BeforeAll
     static void addUsers(
             @Autowired MockMvc mockMvc,
@@ -70,12 +101,12 @@ class IMControllerTest {
         registerRequest.setUsername("testAsker5");
         registerRequest.setPassword(passwordEncoder.encode(password));
         registerRequest.setEmail(email);
-        User asker = new User(registerRequest);
+        asker = new User(registerRequest);
         userRepository.save(asker);
         askerId = asker.getId();
 
         registerRequest.setUsername("testAnswerer5");
-        User answerer = new User(registerRequest);
+        answerer = new User(registerRequest);
         answerer.setRole(UserRole.ANSWERER);
         userRepository.save(answerer);
         answererId = answerer.getId();
@@ -87,6 +118,19 @@ class IMControllerTest {
         Admin admin = new Admin(adminRequest);
         adminRepository.save(admin);
 
+        registerRequest.setUsername("testAsker6");
+        registerRequest.setPassword(passwordEncoder.encode(password));
+        registerRequest.setEmail(email);
+        User asker2 = new User(registerRequest);
+        userRepository.save(asker2);
+        askerId2 = asker2.getId();
+
+        registerRequest.setUsername("testAnswerer6");
+        User answerer2 = new User(registerRequest);
+        answerer2.setRole(UserRole.ANSWERER);
+        userRepository.save(answerer2);
+        answererId2 = answerer2.getId();
+
         LoginRequest userRequest = new LoginRequest();
         userRequest.setUsername("testAsker5");
         userRequest.setPassword(password);
@@ -96,6 +140,15 @@ class IMControllerTest {
         userRequest.setPassword(password);
         answererToken = mockUtils.postAndDeserialize("/api/user/login", askerToken, userRequest, status().isOk(), TokenResponse.class).getToken();
 
+        LoginRequest user2Request = new LoginRequest();
+        user2Request.setUsername("testAsker6");
+        user2Request.setPassword(password);
+        askerToken2 = mockUtils.postAndDeserialize("/api/user/login", askerToken, user2Request, status().isOk(), TokenResponse.class).getToken();
+
+        user2Request.setUsername("testAnswerer6");
+        user2Request.setPassword(password);
+        answererToken2 = mockUtils.postAndDeserialize("/api/user/login", askerToken, user2Request, status().isOk(), TokenResponse.class).getToken();
+
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername(SecurityConstants.SUPER_ADMIN_USERNAME);
         loginRequest.setPassword(SecurityConstants.SUPER_ADMIN_PASSWORD);
@@ -104,11 +157,33 @@ class IMControllerTest {
     }
 
     @Test
-    void sendMessage() {
+    void sendMessage() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("aa");
+        user.setPassword("aaaa");
+        user.setRole(UserRole.ANSWERER);
+        long id = 1L;
+        ZonedDateTime sendTime = ZonedDateTime.now();
+        User sender = user;
+        OrderRequest request = new OrderRequest();
+        request.setAsker(askerId);
+        request.setAnswerer(answererId);
+        request.setTitle(question);
+        request.setDescription(description);
+        Order order = new Order(request, asker, answerer, true);
+        orderRepo.save(order);
+        imService.sendFromUser(order, asker, "1234567");
+        Order order2 = new Order(request, asker, answerer, true);
+        orderRepo.save(order2);
+        imService.getOrderHistoryMessages(order);
+        imService.getOrderHistoryMessages(order.getId());
 
-    }
+        mockUtils.getUrl("/im/history/" + order.getId(), askerToken, null,null,  status().isOk());
+        mockUtils.getUrl("/im/history/" + order.getId(), answererToken, null,null,  status().isOk());
 
-    @Test
-    void getHistory() {
+//        mockUtils.getUrl("/im/history/" + order.getId(), askerToken2, null, null, status().isForbidden());
+//        mockUtils.getUrl("/im/history/" + order.getId(), null, null,null,  status().isUnauthorized());
+//        mockUtils.getUrl("/im/history/" + 1000, askerToken, null,null,  status().isNotFound());
     }
 }
