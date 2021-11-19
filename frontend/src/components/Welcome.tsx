96e423dc068b729f0a4d022a91c2eecd79b05190
刘松铭 <2018011960@secoder.net>
2021-11-19 22:50:12 +0800

    (fix) (frontend #154): open privacy in a new tab and fix the display of msgList in Welcome

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -16,7 +16,7 @@ import {
     List,
     ListItem,
     ListItemIcon,
-    ListItemText,
+    ListItemText, Stack,
 } from "@mui/material";
 import IncomeStatistics from "./IncomeStatistics";
 import Avatar from "@mui/material/Avatar";
@@ -37,6 +37,7 @@ import MoneyIcon from "@mui/icons-material/Money";
 import AnswererList from "./AnswererList";
 import Library from "./Library";
 import NotificationList from "./NotificationList";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
 export default function Welcome() {
     const theme = useTheme();
@@ -331,9 +332,15 @@ export default function Welcome() {
                             {user ? (
                                 <>
                                     <Typography align="left" variant="body2">
-                                        下方显示了您的消息列表
+                                        这是您的消息列表，
+                                        <Link
+                                            variant="body2"
+                                            component={RouterLink}
+                                            to="/notif"
+                                        >
+                                            点此查看更多
+                                        </Link>
                                     </Typography>
-                                    <NotificationList compact />
                                 </>
                             ) : (
                                 <Typography align="left" variant="body2">
@@ -351,6 +358,20 @@ export default function Welcome() {
                         </>
                     }
                 />
+                <CardContent>
+                    {user ?
+                        <NotificationList compact /> :
+                        <Stack direction="column"
+                               justifyContent="center"
+                               alignItems="center"
+                               spacing={2}>
+                            < ErrorOutlineIcon color="warning" sx={{fontSize: 60}} />
+                            <Typography variant={"h6"} mt={1} mb={4}>
+                                {"请登录后重试"}
+                            </Typography>
+                        </Stack>
+                    }
+                </CardContent>
             </Card>
         );
     };
