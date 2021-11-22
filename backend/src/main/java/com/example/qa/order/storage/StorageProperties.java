77990722d4cf5be6ad8256d78aa18cbe4ac2fa09
肖震宇 <2019011237@secoder.net>
2021-11-22 19:44:04 +0800

    (develop) (backend #157): add upload pictures api

--- backend/src/main/java/com/example/qa/order/storage/StorageProperties.java
+++ backend/src/main/java/com/example/qa/order/storage/StorageProperties.java
@@ -10,9 +10,18 @@ public class StorageProperties {
      * Folder location for storing files
      */
     private String location = "upload-dir";
+    private String locationPic = "upload-pic";
 
     public String getLocation() {
         return location;
+    }
+
+    public String getLocationPic(){
+        return locationPic;
+    }
+
+    public void setLocationPic(String location){
+        this.locationPic = location;
     }
 
     public void setLocation(String location) {
--- backend/src/main/java/com/example/qa/order/storage/StorageService.java
+++ backend/src/main/java/com/example/qa/order/storage/StorageService.java
@@ -11,10 +11,14 @@ public interface StorageService {
 
     void init();
     void store(MultipartFile file, UUID uuid);
+    void storePic(MultipartFile file, UUID uuid);
     Stream<Path> loadAll();
     Path load(UUID uuid);
+    Path loadPic(UUID uuid);
     Resource loadAsResource(UUID uuid);
+    Resource loadAsResourcePic(UUID uuid);
     void deleteAll();
     void delete(UUID uuid);
     String getNameByUUID(UUID uuid);
+    String getNameByUUIDPic(UUID uuid);
 }
